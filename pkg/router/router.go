43de7a9fe79a01af4a3d242995a6fc7a5c54d10c
quangtung <quangtung@dytechlab.com>
2024-05-16 16:10:01 +0700

    Add HTTP Post

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -57,29 +57,68 @@ func HTMLGet[T any, Req any](
 		ctx := NewContext(writer, request)
 		var req Req
 
-		pathParams := pattern.GetPathParams()
-		err := assignParams(&req, pattern.GetAllParams(), func(key string) string {
-			if inList(pathParams, key) {
-				return chi.URLParam(ctx.Request, key)
-			}
-			return ctx.Request.URL.Query().Get(key)
-		})
-		if err != nil {
-			r.writeResponse(ctx, "", err)
+		if err := doAssignParams(ctx, &req, pattern); err != nil {
+			r.writeResponse(ctx, "", err, http.StatusBadRequest)
 			return
 		}
 
 		respBody, err := handler(ctx, req)
-		r.writeResponse(ctx, respBody, err)
+		r.writeResponse(ctx, respBody, err, http.StatusInternalServerError)
 	})
 }
 
-func (r *Router) writeResponse(ctx *Context, respBody template.HTML, err error) {
+func HTMLPost[T any, Req any](
+	r *Router, pattern urls.Path[T],
+	handler func(ctx *Context, req Req) (template.HTML, error),
+) {
+	htmlChangeAction(r, r.mux.Post, pattern, handler)
+}
+
+func htmlChangeAction[T any, Req any](
+	r *Router,
+	actionFn func(pattern string, handler http.HandlerFunc),
+	pattern urls.Path[T],
+	handler func(ctx *Context, req Req) (template.HTML, error),
+) {
+	var testPathVal T
+	var testReqVal Req
+	urls.CheckIsSubStruct(testReqVal, testPathVal)
+
+	actionFn(pattern.GetPattern(), func(writer http.ResponseWriter, request *http.Request) {
+		ctx := NewContext(writer, request)
+
+		var req Req
+		if err := json.NewDecoder(request.Body).Decode(&req); err != nil {
+			r.writeResponse(ctx, "", err, http.StatusBadRequest)
+			return
+		}
+
+		if err := doAssignParams(ctx, &req, pattern); err != nil {
+			r.writeResponse(ctx, "", err, http.StatusBadRequest)
+			return
+		}
+
+		respBody, err := handler(ctx, req)
+		r.writeResponse(ctx, respBody, err, http.StatusInternalServerError)
+	})
+}
+
+func doAssignParams[T any](ctx *Context, req any, pattern urls.Path[T]) error {
+	pathParams := pattern.GetPathParams()
+	return assignParams(req, pattern.GetAllParams(), func(key string) string {
+		if inList(pathParams, key) {
+			return chi.URLParam(ctx.Request, key)
+		}
+		return ctx.Request.URL.Query().Get(key)
+	})
+}
+
+func (r *Router) writeResponse(ctx *Context, respBody template.HTML, err error, status int) {
 	writer := ctx.Writer
 
 	if err != nil {
 		writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-		writer.WriteHeader(http.StatusInternalServerError)
+		writer.WriteHeader(status)
 		_ = json.NewEncoder(writer).Encode(ErrorBody{
 			Error: err.Error(),
 		})
--- pkg/router/router_test.go
+++ pkg/router/router_test.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"bytes"
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"html/template"
@@ -143,39 +144,39 @@ type userID int64
 type userParams struct {
 	UserID userID `json:"user_id"`
 	Search string `json:"search"`
+	Age    int    `json:"age"`
 }
 
 type userGetRequest struct {
 	UserID userID `json:"user_id"`
 	Search string `json:"search"`
+	Age    int    `json:"age"`
 }
 
 var userPath = urls.New[userParams]("/api/users/{user_id}")
 
 func TestHTMLGet_Success(t *testing.T) {
-	t.Run("normal", func(t *testing.T) {
-		r := NewRouter()
+	r := NewRouter()
 
-		var inputReq userGetRequest
-		HTMLGet(r, userPath, func(ctx *Context, req userGetRequest) (template.HTML, error) {
-			inputReq = req
-			return "<div>Hello</div>", nil
-		})
-
-		req := httptest.NewRequest(http.MethodGet, "/api/users/123", nil)
-		writer := httptest.NewRecorder()
-		r.Mux().ServeHTTP(writer, req)
-
-		assert.Equal(t, userGetRequest{
-			UserID: 123,
-		}, inputReq)
-
-		assert.Equal(t, http.StatusOK, writer.Code)
-		assert.Equal(t, "<div>Hello</div>", writer.Body.String())
-		assert.Equal(t, http.Header{
-			"Content-Type": []string{"text/html; charset=utf-8"},
-		}, writer.Header())
+	var inputReq userGetRequest
+	HTMLGet(r, userPath, func(ctx *Context, req userGetRequest) (template.HTML, error) {
+		inputReq = req
+		return "<div>Hello</div>", nil
 	})
+
+	req := httptest.NewRequest(http.MethodGet, "/api/users/123", nil)
+	writer := httptest.NewRecorder()
+	r.Mux().ServeHTTP(writer, req)
+
+	assert.Equal(t, userGetRequest{
+		UserID: 123,
+	}, inputReq)
+
+	assert.Equal(t, http.StatusOK, writer.Code)
+	assert.Equal(t, "<div>Hello</div>", writer.Body.String())
+	assert.Equal(t, http.Header{
+		"Content-Type": []string{"text/html; charset=utf-8"},
+	}, writer.Header())
 }
 
 func TestHTMLGet_Not_A_Sub_Struct(t *testing.T) {
@@ -216,6 +217,119 @@ func TestHTMLGet_With_Error(t *testing.T) {
 
 	assert.Equal(t, http.StatusInternalServerError, writer.Code)
 	assert.Equal(t, `{"error":"some handler error"}`+"\n", writer.Body.String())
+	assert.Equal(t, http.Header{
+		"Content-Type": []string{"application/json; charset=utf-8"},
+	}, writer.Header())
+}
+
+func TestHTMLGet_With_Invalid_Query_Param(t *testing.T) {
+	r := NewRouter()
+
+	HTMLGet(r, userPath, func(ctx *Context, req userGetRequest) (template.HTML, error) {
+		return "<div>Hello</div>", nil
+	})
+
+	req := httptest.NewRequest(
+		http.MethodGet,
+		"/api/users/123?age=AB",
+		nil,
+	)
+	writer := httptest.NewRecorder()
+	r.Mux().ServeHTTP(writer, req)
+
+	assert.Equal(t, http.StatusBadRequest, writer.Code)
+	assert.Equal(t,
+		`{"error":"router: can not parse value 'AB' into field 'Age'"}`+"\n",
+		writer.Body.String(),
+	)
+	assert.Equal(t, http.Header{
+		"Content-Type": []string{"application/json; charset=utf-8"},
+	}, writer.Header())
+}
+
+type userSetting struct {
+	Path  string `json:"path"`
+	Count int    `json:"count"`
+}
+
+type userPostRequest struct {
+	UserID  userID      `json:"user_id"`
+	Search  string      `json:"search"`
+	Age     int         `json:"age"`
+	Body    string      `json:"body"`
+	Setting userSetting `json:"setting"`
+}
+
+func TestHTMLPost_Success(t *testing.T) {
+	r := NewRouter()
+
+	var inputReq userPostRequest
+	HTMLPost(r, userPath, func(ctx *Context, req userPostRequest) (template.HTML, error) {
+		inputReq = req
+		return "<div>Hello</div>", nil
+	})
+
+	body := `
+{
+  "user_id": 33,
+  "search": "search text",
+  "body": "Some Body",
+  "setting": {
+    "path": "some path",
+    "count": 8899
+  }
+}
+`
+
+	req := httptest.NewRequest(http.MethodPost, "/api/users/123", bytes.NewBufferString(body))
+	writer := httptest.NewRecorder()
+	r.Mux().ServeHTTP(writer, req)
+
+	assert.Equal(t, userPostRequest{
+		UserID: 123,
+		Search: "search text",
+		Body:   "Some Body",
+		Setting: userSetting{
+			Path:  "some path",
+			Count: 8899,
+		},
+	}, inputReq)
+
+	assert.Equal(t, http.StatusOK, writer.Code)
+	assert.Equal(t, "<div>Hello</div>", writer.Body.String())
+	assert.Equal(t, http.Header{
+		"Content-Type": []string{"text/html; charset=utf-8"},
+	}, writer.Header())
+}
+
+func TestHTMLPost_Parse_JSON_Request_Error(t *testing.T) {
+	r := NewRouter()
+
+	HTMLPost(r, userPath, func(ctx *Context, req userPostRequest) (template.HTML, error) {
+		return "<div>Hello</div>", nil
+	})
+
+	body := `
+{
+  "user_id": 33,
+  "search": "search text",
+  "body": "Some Body",
+  "setting": {
+    "path": "some path",
+    "count": "mm"
+  }
+}
+`
+
+	req := httptest.NewRequest(http.MethodPost, "/api/users/123", bytes.NewBufferString(body))
+	writer := httptest.NewRecorder()
+	r.Mux().ServeHTTP(writer, req)
+
+	assert.Equal(t, http.StatusBadRequest, writer.Code)
+	assert.Equal(t,
+		`{"error":"json: cannot unmarshal string into Go struct field userSetting.setting.count of type int"}`+"\n",
+		writer.Body.String(),
+	)
 	assert.Equal(t, http.Header{
 		"Content-Type": []string{"application/json; charset=utf-8"},
 	}, writer.Header())
